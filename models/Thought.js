8e83343d8f610f2dbdcf147d2bf68f3a885593e7
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-08 16:44:37 -0400

    defined thoughts model

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('..models/Reaction');
+const dayjs = require('dayjs');
+const { stringify } = require('querystring');
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            trim: true, 
+            minLength: 1,
+            maxLength: 200
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtTime) => dayjs(createdAtTime).format('MMMM D, YYYY h:mm A')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thoughts;
