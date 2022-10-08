f75dc0f848fd98d8c98a5ed41550ae4e8e3f7bd0
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-08 18:49:12 -0400

    moved api routes into api folder. index.js to wrap around all api routes and exported.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const apiRoutes = require('./api');
 
-router.use('/user', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+router.use('./api', apiRoutes);
 
-module.export = router;
+router.use((req, res) => res.send('Wrong route!'));
+
+module.exports = router;
